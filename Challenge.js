e935830250814a9d395f74d96124561a6a7ac291
unknown <guido_181193@outlook.com>
2020-07-27 03:32:19 -0300

    Análisis Diagonal listo

--- Challenge.js
+++ Challenge.js
@@ -1,5 +1,5 @@
 function isMutant(dna) {
-  var cant_cadena_mutante = 0;
+  var cant_cadenas_mutantes = 0;
 
   function secuencia_igual(secuencia) {
     //A partir de un string, devuelve "true" si hay 4 letras iguales y consecutivas
@@ -34,11 +34,11 @@ function isMutant(dna) {
   //Análisis "Horizontal" de la Secuencia
   function analisisHorizontal(dna) {
     for (j = 0; j < dna.length; j++) {
-      if (cant_cadena_mutante >= 2) {
+      if (cant_cadenas_mutantes >= 2) {
         break;
       }
       if (secuencia_igual(dna[j]) === true) {
-        cant_cadena_mutante += 1;
+        cant_cadenas_mutantes += 1;
       }
     }
   }
@@ -46,21 +46,39 @@ function isMutant(dna) {
   analisisHorizontal(dna);
 
   //Análisis "Vertical" de la Secuencia
-  function generar_dna_vertical(dna) {
+  function dividir_caracteres(dna) {
+    //input  ["ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"]
+    /*output [  ["A", "T", "G", "C", "G", "A"],
+                ["C", "A", "G", "T", "G", "C"],
+                ["T", "T", "A", "T", "G", "T"],
+                ["A", "G", "A", "A", "G", "G"],
+                ["C", "C", "C", "C", "T", "A"],
+                ["T", "C", "A", "C", "T", "G"]  ]*/
     var z = [];
     var w = [];
-    var nueva_cadena_array = [];
-    var dna_vertical = "";
-    var array_verticales = [];
 
     for (x of dna) {
       w = x.split("");
       z.push(w);
     }
 
-    for (k = 0; k < z.length; k++) {
-      for (l = 0; l < z.length; l++) {
-        nueva_cadena_array.push(z[l][k]);
+    return z;
+  }
+
+  var caracteres = dividir_caracteres(dna);
+
+  function generar_dna_vertical(dna) {
+    //input  ["ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"]
+    //ver f(x) dividir_caracteres
+    //output ["ACTACT","TATGCC","GGAACA","CTTACC","GGGGTT","ACTGAG"]
+
+    var nueva_cadena_array = [];
+    var dna_vertical = "";
+    var array_verticales = [];
+
+    for (k = 0; k < caracteres.length; k++) {
+      for (l = 0; l < caracteres.length; l++) {
+        nueva_cadena_array.push(caracteres[l][k]);
       }
       for (m of nueva_cadena_array) {
         dna_vertical = dna_vertical + m;
@@ -73,74 +91,65 @@ function isMutant(dna) {
     return array_verticales;
   }
 
-  analisisHorizontal(generar_dna_vertical(dna));
+  if (cant_cadenas_mutantes < 2) {
+    analisisHorizontal(generar_dna_vertical(dna));
+  }
 
+  //Análisis "Diagonal" de la Secuencia
+  function generar_dna_diagonal(dna) {
+    var coordenadas_iniciales = [[0, 0]];
+    var coordenadas_completas = [];
+    var palabra_diagonal = "";
+    var array_diagonal = [];
 
-  //Resultado
-  if (cant_cadena_mutante >= 2) {
+    for (a = 1; a < caracteres.length - 3; a++) {
+      coordenadas_iniciales.push([0, a]);
+      coordenadas_iniciales.push([a, 0]);
+    }
+
+    for (d of coordenadas_iniciales) {
+      var control = d[1];
+      switch (d[0]) {
+        case 0: //x = 0 tienen que llegar hasta y = length - 1 triángulo inferior
+          control = d[1];
+          break;
+        default:
+          // x <> 0 tiene que llegar hasta  x = length - 1 triángulo superior
+          control = d[0];
+          break;
+      }
+      var indice = 0;
+      coordenadas_completas.push(d);
+
+      while (control < caracteres.length - 1) {
+        coordenadas_completas.push([
+          coordenadas_completas[indice][0] + 1,
+          coordenadas_completas[indice][1] + 1,
+        ]);
+        control += 1;
+        indice += 1;
+      }
+      
+      for (t of coordenadas_completas) {
+        palabra_diagonal = palabra_diagonal + caracteres[t[1]][t[0]];
+      }
+
+      array_diagonal.push(palabra_diagonal);
+      coordenadas_completas = [];
+      palabra_diagonal = "";
+    }
+    
+    return array_diagonal;
+  }
+
+  if (cant_cadenas_mutantes < 2) {
+    analisisHorizontal(generar_dna_diagonal(dna));
+  }
+
+  //Resultado: ¿Es un mutante?
+  if (cant_cadenas_mutantes >= 2) {
     return true;
   } else {
     return false;
   }
 }
-
-/*
-for (i = 0; i < letras.length - 4; i++) {
-  if (comparar_son_iguales(letras[i], letras[i + 3])) {
-    if (comparar_son_iguales(letras[i + 1], letras[i + 2])) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
-  else { 
-    return false;
-  }  
-}
-*/
-
-/*function conteo_letra(secuencia) {
-  //Cuenta cuántas veces aparece una letra en el string
-  var letras = [];
-  var A = 0;
-  var T = 0;
-  var G = 0;
-  var C = 0;
-
-  letras = secuencia[i].split(""); //Separo los caracteres
-
-  for (j = 0; j < letras.legth; j++) {
-    //Cuento las letras de cada string
-    switch (letras[j]) {
-      case "A":
-        A++;
-        break;
-      case "a":
-        A++;
-        break;
-      case "T":
-        T++;
-        break;
-      case "t":
-        T++;
-        break;
-      case "G":
-        G++;
-        break;
-      case "g":
-        G++;
-        break;
-      case "C":
-        C++;
-        break;
-      case "c":
-        C++;
-        break;
-    }
-  }
-
-  if (A === 4 || T === 4 || G === 4 || C === 4) {
-    return true;
-  }
-}*/
